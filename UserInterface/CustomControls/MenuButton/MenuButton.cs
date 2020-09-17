bba229fbc5e659569e479d09295bbdabc6ff2c27
Ionut Andrei <ionut.tivadar@gmail.com>
2020-09-17 22:19:02 +0300

    Adding custom controls

---
+++ UserInterface/CustomControls/MenuButton/MenuButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace UserInterface.CustomControls.MenuButton
+
+{
+    public class MenuButton : Button
+    {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(nameof(Image),
+                                                                                              typeof(ImageSource),
+                                                                                              typeof(MenuButton),
+                                                                                              new PropertyMetadata(null));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+    }
+
+}
---
+++ UserInterface/CustomControls/SliderControl/Slider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace UserInterface.CustomControls
+{
+    public class Slider : ItemsControl
+    {
+        private const int RowCount = 4;
+        private byte[,] _state;
+
+        public Slider()
+        {
+            Tiles = new ObservableCollection<UIElement>();
+            InitState();
+            InitSlider();
+            
+        }
+        
+
+       
+        public ObservableCollection<UIElement> Tiles  { get; set;  }
+
+
+        private void InitState()
+        {
+            _state = new byte[RowCount, RowCount];
+            for (byte tileIndex = 0; tileIndex < RowCount * RowCount; tileIndex++)
+            {
+                _state[tileIndex / RowCount,tileIndex % RowCount] = tileIndex;
+            }
+        }
+
+        private void InitSlider()
+        {
+            Canvas c = new Canvas()
+            {
+                Background = Brushes.White,
+                Width = 50,
+                Height = 50,
+            };
+            Tiles.Add(c);
+            ItemsSource = Tiles;
+        }
+    }
+}
