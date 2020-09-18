04834533acf5072e4d9ff2c287b0fa40e2c54772
Ionut Andrei <ionut.tivadar@gmail.com>
2020-09-18 21:46:02 +0300

    Added TIle ccontrol

---
+++ UserInterface/CustomControls/Tile/Tile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UserInterface.CustomControls
+{
+    class Tile :  Canvas
+    {
+            public Thickness FinalMargin { get; set; }
+    }
+}
