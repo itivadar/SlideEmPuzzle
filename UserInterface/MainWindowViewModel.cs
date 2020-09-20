f747c42e1893e9ab129a1dc0f388f1dd4b803cfa
Ionut Andrei <ionut.tivadar@gmail.com>
2020-09-20 20:37:31 +0300

    Adding new pages.
    Added Frameview control to the main window and the empty About page.

--- UserInterface/MainWindowViewModel.cs
+++ UserInterface/MainWindowViewModel.cs
@@ -13,27 +13,39 @@ namespace UserInterface
     class MainWindowViewModel : BindableBase
     {
 
-        Button button;
-
         private Uri _mainFrame;
+        private ListViewItem _selectedMenu;
 
         public Uri MainFrame
         {
             get => _mainFrame;
-            set 
+            set
             {
                 _mainFrame = value;
                 RaisePropertyChanged(nameof(MainFrame));
             }
         }
 
+        public ListViewItem SelectedMenu
+        {
+            get => _selectedMenu;
+            set
+            {
+                _selectedMenu = value;
+                RaisePropertyChanged(nameof(SelectedMenu));
+            }
+        }
+
         public ICommand AboutCommand { get; private set; }
         public DelegateCommand ClearCommand { get; private set; }
+        
+
         public MainWindowViewModel()
         {
 
             AboutCommand = new DelegateCommand(this.OpenAboutPage);
             ClearCommand = new DelegateCommand(this.OnClear);
+            OpenAboutPage();
         }
 
         private void OnClear()
