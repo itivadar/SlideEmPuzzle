2effe285682142c1431959c074270cbc749f1220
Ionut Tivadar <ionut.tivadar@gmail.com>
2020-12-30 19:08:57 +0200

    Refactoring.

--- UserInterface/Pages/SliderPage/SliderPageViewModel.cs
+++ UserInterface/Pages/SliderPage/SliderPageViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using SliderPuzzleGenerator;
-using SliderPuzzleSolver;
 using SliderPuzzleSolver.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -14,26 +11,42 @@ namespace UserInterface.Pages.SliderPage
 {
     public class SliderPageViewModel : BindableBase
     {
-        private readonly IPuzzleSolver _puzzleSolver;
-        private readonly IPuzzleGenerator _puzzleGenerator;
+        #region Private Fields
 
-        private DispatcherTimer _timer;
-        private ObservableBoard _sliderState;
-        private int  _playerMoves;
+        private readonly IPuzzleGenerator _puzzleGenerator;
+        private readonly IPuzzleSolver _puzzleSolver;
+        private int _playerMoves;
         private TimeSpan _playerTime;
-        
+        private ObservableBoard _sliderState;
+        private DispatcherTimer _timer;
+
+        #endregion Private Fields
+
+        #region Public Constructors
 
         public SliderPageViewModel(IPuzzleSolver puzzleSolver, IPuzzleGenerator puzzleGenerator)
         {
             _puzzleSolver = puzzleSolver;
             _puzzleGenerator = puzzleGenerator;
-      
+
             RandomizeCommand = new DelegateCommand(OnRandomize);
 
             ConfigureTimer();
         }
 
-        public ICommand RandomizeCommand { get; set; }
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public int PlayerMoves
+        {
+            get => _playerMoves;
+            private set
+            {
+                _playerMoves = value;
+                RaisePropertyChanged(nameof(PlayerMoves));
+            }
+        }
 
         public TimeSpan PlayerTime
         {
@@ -45,26 +58,29 @@ namespace UserInterface.Pages.SliderPage
             }
         }
 
-        public int PlayerMoves
-        {
-            get => _playerMoves;
-            private set
-            {
-                _playerMoves = value;
-                RaisePropertyChanged(nameof(PlayerMoves));
-            }
-            
-        }
+        public ICommand RandomizeCommand { get; set; }
+
         public ObservableBoard SliderState
         {
             get => _sliderState;
-            set 
+            set
             {
                 _sliderState = value;
                 _sliderState.StateChanged -= OnStateChanged;
                 _sliderState.StateChanged += OnStateChanged;
                 RaisePropertyChanged(nameof(SliderState));
             }
+        }
+
+        #endregion Public Properties
+
+        #region Private Methods
+
+        private void ConfigureTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += OnTimerTick;
         }
 
         private void OnRandomize()
@@ -85,13 +101,6 @@ namespace UserInterface.Pages.SliderPage
             }
         }
 
-        private void ConfigureTimer()
-        {
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(1);
-            _timer.Tick += OnTimerTick;
-        }
-
         private void OnTimerTick(object sender, EventArgs e)
         {
             PlayerTime = PlayerTime.Add(TimeSpan.FromSeconds(1));
@@ -102,6 +111,7 @@ namespace UserInterface.Pages.SliderPage
             PlayerMoves = 0;
             PlayerTime = TimeSpan.Zero;
         }
+
+        #endregion Private Methods
     }
 }
-
