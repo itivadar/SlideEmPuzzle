708b4e30d1d0e3c469b7b629606b651590d6557f
Ionut Tivadar <ionut.tivadar@gmail.com>
2021-01-25 21:00:10 +0200

    Added single button to the GameOverPage.

--- UserInterface/Pages/SliderPage/SliderPageViewModel.cs
+++ UserInterface/Pages/SliderPage/SliderPageViewModel.cs
@@ -108,7 +108,6 @@ namespace UserInterface.Pages.SliderPage
         {
             //starts the timer only when the page displayed to avoid delays.
             StartGame();
-            var bestMoves = _puzzleSolver.SolutionSteps(_sliderState.Board);
         }
 
         #endregion Public
