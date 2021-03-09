cd8015c4c6a529f85cb7f8e194bc294fe38acf6f
Ionut Tivadar <ionut.tivadar@gmail.com>
2021-03-10 00:22:01 +0200

    Improve the puzzle generator module.

--- SliderPuzzleGenerator/PuzzleGenerator.cs
+++ SliderPuzzleGenerator/PuzzleGenerator.cs
@@ -5,124 +5,169 @@ using System.Collections.Generic;
 
 namespace SliderPuzzleGenerator
 {
-    public class PuzzleGenerator : IPuzzleGenerator
-    {
-        private Random _rand;
+	public class PuzzleGenerator : IPuzzleGenerator
+	{
+		/// <summary>
+		/// Enum for different board types
+		/// </summary>
+		private enum BoardType
+		{
+			Easy,
+			Medium,
+			Hard
+		}
 
-        //Predefined 2 x 2 boards
-        private readonly string[] _2x2Boards =
-        {
-          "0 3 2 1",
-          "3 1 2 0",
-          "2 3 0 1",
-          "3 0 2 1",
-          "2 3 1 0",
-        };
+		#region Private Fields
 
-        //Predefined 3 X 3 starting boards
-        private readonly string[] _3x3Boards =
-        {
-          "4 1 2 3 0 6 5 7 8",
-          "6 0 8 4 3 5 1 2 7",
-          "6 4 7 8 5 0 3 2 1", //31 moves
+		/// <summary>
+		/// Array of predefined 2x2 boards.
+		/// </summary>
+		private static readonly string[] EasyBoards =
+		{
+					"0 3 2 1",
+					"3 1 2 0",
+					"2 3 0 1",
+					"3 0 2 1",
+					"2 3 1 0",
+				};
+
+		/// <summary>
+		/// Array of predefined 3x3 boards.
+		/// </summary>
+		private static readonly string[] MediumBoards =
+		{
+					"4 1 2 3 0 6 5 7 8",
+					"6 0 8 4 3 5 1 2 7",
           "4 1 2 3 0 6 5 7 8", //22 moves
-          "1 0 2 4 6 3 7 5 8"  //5 moves
+          "1 0 2 4 6 3 7 5 8", //5 moves
+					"1 4 3 7 0 8 6 5 2", //18 moves
+					"1 3 5 7 2 6 8 0 4", //11 moves
         };
 
-        //Predefined 4 X 4 starting boards
-        private readonly string[] _4x4Boards =
-        {
-          "1 2 4 12 5 6 3 0 9 10 8 7 13 14 11 15", //10 moves
-          "1 2 3 4 5 6 7 8 10 0 11 12 9 13 14 15", //5 moves
-          "2 3 4 0 1 6 7 8 5 10 11 12 9 13 14 15", //9 moves
+		/// <summary>
+		/// Array of predefined 4x4 boards.
+		/// </summary>
+		private static readonly string[] HardBoards =
+		{
           "1 2 8 3 5 11 6 4 0 10 7 12 9 13 14 15", //14 moves
-          "1 2 3 4 6 10 7 8 5 0 11 12 9 13 14 15", //7 moves
-          "6 3 7 4 2 9 10 8 1 5 12 15 13 0 14 11" //20 moves
+					"5 1 3 4 9 2 7 8 13 0 10 12 14 6 11 15", //11 moves
+					"5 1 2 4 9 6 3 7 13 10 0 8 14 15 11 12", //12 moves
+					"5 2 4 0 6 1 3 8 13 11 7 12 10 9 14 15", //17 moves 
+					"6 3 7 4 2 9 10 8 1 5 12 15 13 0 14 11", //20 moves
+					"1 4 8 3 7 2 10 11 5 6 0 15 9 13 14 12", //22 moves
         };
 
-        //We map last selected position to each dimension to avoid generating same board twice
-        private Dictionary<int, int> _lastRandomIndexByDimension;
+		//We map last selected position to each dimension to avoid generating same board twice
+		private readonly Dictionary<BoardType, int> _lastChosenIndexByDimension;
 
-        public PuzzleGenerator()
-        {
-            _rand = new Random();
-            _lastRandomIndexByDimension = new Dictionary<int, int>()
-            {
-                { 2, -1 },
-                { 3, -1 },
-                { 4, -1 }
-            };
-        }
+		private readonly Random _rand;
 
-        //generate a random puzzle by starting from a  predefined position which is randomly selected
-        //make legal moves until it is a more random solution
-        public IBoard GenerateRandomPuzzle(int dimension)
-        {
-            return dimension switch
-            {
-                2 => Generate2x2Board(),
-                3 => Generate3x3Board(),
-                4 => Generate4x4Board(),
-                _ => null,
-            };
-        }
+		#endregion Private Fields
 
-        //for 2 X 2 boards it is enough to pick one randomly from the possible board99s;
-        private IBoard Generate2x2Board()
-        {
-            var randIndex = _rand.Next(_2x2Boards.Length);
+		#region Public Constructors
 
-            //choose next if generated last time
-            if (randIndex == _lastRandomIndexByDimension[2])
-            {
-                GetNextIndex(_2x2Boards.Length, ref randIndex);
-            }
+		public PuzzleGenerator()
+		{
+			_rand = new Random();
+			_lastChosenIndexByDimension = new Dictionary<BoardType, int>();
+		}
 
-            _lastRandomIndexByDimension[2] = randIndex;
-            return new Board(_2x2Boards[randIndex]);
-        }
+		#endregion Public Constructors
 
-        //for 3 X 3 puzzle start from a random position from the predefined
-        //move the blank randomly
-        private IBoard Generate3x3Board()
-        {
-            return GetRandomBoard(3, _3x3Boards, 8);
-        }
+		#region Public Methods
 
-        //for 4 X 4 puzzle start from a random position from the predefined
-        //move the blank randomly
-        private IBoard Generate4x4Board()
-        {
-            return GetRandomBoard(4, _4x4Boards, 6);
-        }
+		/// <summary>
+		///Generate a random puzzle by starting from a  predefined position which is randomly selected
+		/// Makes legal moves until it is a more random solution
+		/// </summary>
+		/// <param name="dimension">The dimension of the requested puzzle</param>
+		/// <returns>a random puzzle represented by <see cref="IBoard"/></returns>
+		public IBoard GenerateRandomPuzzle(int dimension)
+		{
+			return dimension switch
+			{
+				2 => GenerateEasyBoard(),
+				3 => GenerateMediumBoard(),
+				4 => GenerateHardBoard(),
+				_ => null,
+			};
+		}
 
-        //generates a random puzzle starting from a board randomly selected from the boards space
-        //moves the blank tile
-        private IBoard GetRandomBoard(int dimension, string[] boardsSpace, int movesCount)
-        {
-            var startingBoardIndex = _rand.Next(boardsSpace.Length);
+		#endregion Public Methods
 
-            //choose next if same board generated last time
-            if (startingBoardIndex == _lastRandomIndexByDimension[dimension])
-            {
-                GetNextIndex(boardsSpace.Length, ref startingBoardIndex);
-            }
+		#region Private Methods
 
-            IBoard currentBoard = new Board(boardsSpace[startingBoardIndex]);
-            for (int move = 0; move < movesCount; move++)
-            {
-                var neighbors = currentBoard.GetNeighbordBoards();
-                currentBoard = neighbors[_rand.Next(neighbors.Count)];
-            }
+		/// <summary>
+		/// Generate an easy 2x2 board by picking randomly one board from the easy boards.
+		/// </summary>
+		/// <returns>an random easy board</returns>
+		private IBoard GenerateEasyBoard()
+		{
+			var randIndex = GetNextIndex(EasyBoards, BoardType.Easy);
+			return new Board(EasyBoards[randIndex]);
+		}
 
-            if (currentBoard.IsSolved()) return GetRandomBoard(dimension, boardsSpace, movesCount);
-            _lastRandomIndexByDimension[dimension] = startingBoardIndex;
-            return currentBoard;
-        }
+		/// <summary>
+		/// Generates a medium 3x3 by picking randomly from the predefined boards.
+		/// Moves the blank tile in random directions.
+		/// </summary>
+		/// <returns>an random medium board</returns>
+		private IBoard GenerateMediumBoard()
+		{
+			return GetRandomBoard(MediumBoards, BoardType.Medium, 8);
+		}
 
-        private void GetNextIndex(int arrayLength, ref int randIndex)
-        {
-            randIndex = (randIndex + 1) % arrayLength;
-        }
-    }
+		/// <summary>
+		/// Generates an hard 4x4 by picking randomly from the predefined boards.
+		/// Moves the blank tile in random directions.
+		/// </summary>
+		/// <returns>an random hard board</returns>
+		private IBoard GenerateHardBoard()
+		{
+			return GetRandomBoard(HardBoards, BoardType.Hard, 6);
+		}
+
+		/// <summary>
+		/// Choose a random index to pick a board from the specific board array
+		/// </summary>
+		/// <param name="boards">The boards array.</param>
+		/// <param name="boardType">The board type to be picked</param>
+		/// <returns>an integer representing the index of the board that should be picked.</returns>
+		private int GetNextIndex(string[] boards, BoardType boardType)
+		{
+			var randIndex = _rand.Next(boards.Length);
+			if (_lastChosenIndexByDimension.ContainsKey(boardType) &&
+					randIndex == _lastChosenIndexByDimension[boardType])
+			{
+				randIndex = (randIndex + 1) % boards.Length;
+			}
+			_lastChosenIndexByDimension[boardType] = randIndex;
+			return randIndex;
+		}
+
+		/// <summary>
+		/// Generates a random puzzle starting from a board randomly selected from the boards space
+		/// Moves the blank tile
+		/// </summary>
+		/// <param name="boardsSpace">The board array.</param>
+		/// <param name="boardType">The type of the board.</param>
+		/// <param name="movesCount">The moves made by the blank tile</param>
+		/// <returns>a random board</returns>
+		private IBoard GetRandomBoard(string[] boardsSpace, BoardType boardType, int movesCount)
+		{
+			var startingBoardIndex = GetNextIndex(boardsSpace, boardType);
+
+			IBoard currentBoard = new Board(boardsSpace[startingBoardIndex]);
+			for (int move = 0; move < movesCount; move++)
+			{
+				var neighbors = currentBoard.GetNeighbordBoards();
+				currentBoard = neighbors[_rand.Next(neighbors.Count)];
+			}
+
+			if (currentBoard.IsSolved()) return GetRandomBoard(boardsSpace, boardType, movesCount);
+			return currentBoard;
+		}
+
+		#endregion Private Methods
+	}
 }
